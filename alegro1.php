0a37b5611775f5c700cb7a7efec196f5ed2db29d
likearockets <aczech81@gmail.com>
2017-11-22 22:49:03 +0100

    Add files via upload

---
+++ alegro1.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Komis używanych samochodów</title>
+        <link rel="stylesheet" type="text/css" href="alegro.css">
+    </head>
+    
+        <body>  
+        <div id="container">
+            <div id="header">
+                <div class="logo">
+                    <img src="https://newcdn.iconfinder.com/data/icons/fast-cars-1/512/McLaren_F1_Red-128.png" style="float:left"/>
+                    <span style="color:#c12230">retro</span>car.com
+                    <div style="clear: both"></div>
+                </div>
+            </div>
+
+        <div id="pasek">
+            <ol>
+                  <li><a href="/alegro.php/">Start</a></li>
+                  <li><a href="#">Oferta</a>
+                    <ul>
+                        <li><a href="#samochody">Samochody</a></li>
+                        <li><a href="#mototcykle">Motocykle</a></li>
+                        <li><a href="#inne">Inne</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#marki">Marki</a>
+                      <ul>
+                        <li><a href="#ford">Ford</a></li>
+                        <li><a href="#bmw">BMW</a></li>
+                        <li><a href="#mazda">Mazda</a></li>
+                        <li><a href="#volkswagen">Volkswagen</a></li>
+                        <li><a href="#fiat">Fiat</a></li>
+                    </ul>
+                  </li>
+                <li><a href="#ulubione">Ulubione</a></li>
+            </ol>
+            <div style="clear: both"></div>
+        </div>
+		
+			<div id='oferta'>
+                <p>W naszej ofercie pojawia się wiele marek oraz modeli, które są automatycznie pobierane z naszej rozbudowanej bazy danych.</p>
+				<p>Dzięki poniższej wyszukiwarce znajdą Państwo swój wymarzony samochód!</p>
+			</div>
+                   
+        <div class="stopka">
+            <a href="/onas/">O nas</a> 
+            <a href="/kontakt/">Kontakt</a> 
+            <a href="/pomoc/">Pomoc</a> 
+            <a href="/regulamin/">Regulamin</a>
+            <p>Stworzone przez: Grove Street™</p>
+        </div>
+    </div>     
+  </body>
+</html>
